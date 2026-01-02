167d342c037214de42113662afc8681614c65dcf
Ccdwe <dweckspam@gmail.com>
2026-01-02 13:23:06 +0200

    .

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,13 @@ module.exports = {
   testMatch: ['<rootDir>/tests/**/*.test.(ts|tsx|js)'],
   testPathIgnorePatterns: ['\\.stub\\.test\\.'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  watchPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/android/build/',
+    '<rootDir>/android/app/build/',
+    '<rootDir>/.expo/',
+    '<rootDir>/dist/',
+  ],
   moduleNameMapper: {
     '^expo-router$': '<rootDir>/tests/mocks/expo-router.ts',
     '^expo-notifications$': '<rootDir>/tests/mocks/expo-notifications.ts',
